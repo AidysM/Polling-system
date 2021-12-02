1967d4b9f3afe199784053ed3d55ecd12f93414f
MongushAB <mongush10011979@gmail.com>
2021-12-02 10:22:49 +0700

    created poll, question models

---
+++ polls/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Poll(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    start_date = models.DateField(auto_now_add=True, verbose_name='Дата старта')
+    end_date = models.DateField(verbose_name='Дата окончания')
+    description = models.TextField(verbose_name='Описание')
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+    
+class Question(models.Model):
+    QUESTIONSTYPES = (
+            ('TEXT RESPONSE', 'text response'),
+            ('SINGLECHOICEANSWER', 'single choice answer'), 
+            ('MULTIPLECHOICEANSWER', 'multiple choice answer'),
+        )
+    question_text = models.TextField(verbose_name='Текст вопроса')
+    question_type = models.CharField(max_length=25, choices=QUESTIONSTYPES, verbose_name='Тип вопроса')
+    
+    def __str__(self):
+        return f'{self.question_text}'
+    
+
